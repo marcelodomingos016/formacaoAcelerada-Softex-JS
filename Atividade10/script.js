27e20b88be5d62de0808f6c5d64035b62fad30b0
Marcelo Augusto <93164093+marcelodomingos016@users.noreply.github.com>
2022-10-19 12:53:08 -0300

    Add files via upload
    Projeto de formação acelerada em programação da SOFTEX - Exercicios

---
+++ Atividade10/script.js
@@ -0,0 +1,20 @@
+
+let pessoa = {
+    nome: 'Marcelo',
+    idade: 28,
+    sexo: 'Masculino'
+}
+
+let carro = ['ford', 'Fiat', 'JGM']
+
+function pessoas(arg) {
+    return arg
+}
+function carros(arg) {
+    return arg
+}
+
+console.log(pessoas(pessoa))
+console.log(carros(carro))
+
+
